12282654a597cfc720fad052cd1ae52fb11967e0
Yuessiah <yuessiah@gmail.com>
2017-11-28 01:45:13 +0800

    Update

--- .install.sh
+++ .install.sh
@@ -16,14 +16,10 @@ mv $BASE ~/.vim/
 
 apt-get install ctags
 
-cd ~/.vim/bundle/
-git submodule init Vundle.vim/
-git submodule update Vundle.vim/
-git submodule init YouCompleteMe/
-git submodule update YouCompleteMe/
-cd YouCompleteMe/
+cd ~/.vim/bundle/YouCompleteMe/
 git submodule update --init --recursive
 ./install.py --clang-completer
+git submodule update
 
 # Hard-link the ~/.vimrc from mydotvim's .vimrc
 rm -f ~/.vimrc
